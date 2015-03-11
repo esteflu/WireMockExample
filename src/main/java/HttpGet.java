7660becc4643c8da923cbaace8182acdff4ad94c
stefanlundberg <lundberg.stefan@gmail.com>
2015-03-11 21:29:20 +0100

    Added wiremock example

---
+++ src/main/java/HttpGet.java
@@ -0,0 +1,19 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
+/**
+ * Created by stefanlundberg on 15-03-11.
+ */
+public class HttpGet {
+
+    public String getAsString(String url) throws IOException {
+        return Request.Get(url).execute().returnContent().asString();
+    }
+
+    public HttpResponse getHttpResponse(String url) throws IOException {
+        return Request.Get(url).execute().returnResponse();
+    }
+}
